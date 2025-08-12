16a3b0e41d5f3ad60a213eccf4e3435e20ba39d0
saurav <sauravpanigrahi@protonmail.com>
2025-08-12 11:08:59 +0530

    Update direct_ratings.py
    typo

--- src/rl_env/direct_ratings.py
+++ src/rl_env/direct_ratings.py
@@ -1,4 +1,4 @@
-import src.data_labelling.direct_scorer import score_and_update_batch
+from src.data_labelling.direct_scorer import score_and_update_batch
 from typing import List
 
 def compute_unnormalized_direct_ratings(generations: List[dict], model_path: str, urls: List[str], tokenizer: List[str], worker_id: int):
